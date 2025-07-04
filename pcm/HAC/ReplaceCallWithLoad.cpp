dbd7c22790b55c8012baca9b6f5c6959e9e1ab50
Timo Schneider <timos@timos.inf.ethz.ch>
2025-07-04 18:36:12 +0200

    more work on opt pass

--- pcm/HAC/ReplaceCallWithLoad.cpp
+++ pcm/HAC/ReplaceCallWithLoad.cpp
@@ -1,3 +1,36 @@
+/*
+ * This pass replaces all calls to the functions flow_signal_get, flow_local_state_int_get(), flow_control_get with loads.
+ *
+ * Where we need to load from depends on the device we are compiling for:
+ *   * a device could memory map signals to fixed addresses e.g., maximum rtt
+ *     always lives at 0xdeadbeef in our address space (where 0xdeadbeef is a
+ *     hw constant)
+ *   * a device could memory map signals needed by the handler to a fixed 
+ *     address, i.e., if the handler defines if wants to use maximum rtt as
+ *     signal 3, this signal will be mapped to 0xdeadbeef + 3 
+ *   * a device could memory map signals needed by the handler differently
+ *     for each flow, so we need to get the mapping from the context passed
+ *     to algorithm_main.
+ *   * Different flows might get a private snapshot of the signals or signals
+ *     could be shared and we need to make sure reads/writes are atomic.
+ *
+ * The API can abstract all of this away (in our implementation the
+ * backend / device / driver supplies function pointers for the getter
+ * and setter functions).
+ *
+ * Ideally we have a compiler that can handle many cases, and the vendor
+ * describes each product using a YAML file.
+ *
+ * For now we assume the signals are mapped to a different space for each flow
+ * and are not private (different handler invocations can run on different
+ * cores simultaneously) since a) this is the hardest case and b) this is how
+ * the non-htsim backend works.
+ *
+ * The flow_t struct which is passed to alorithm_main contains a pointer to the
+ * backend_ctx, which in turn contains a pointer to the signals array.
+ * */
+
+
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/IR/PassManager.h"
@@ -5,6 +38,15 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
+
+// the function we are optimizing (algorithm_main(...)) gets multiple arguments
+// we don't touch the first one (ctx) but need to know the positions of the
+// others. In case their position changes, adapt the constants below.
+const int signals_pos = 1;
+const int controls_pos = 2;
+const int thresholds_pos = 3;
+const int local_state_pos = 4;
+const int constants_pos = 5;
 
 using namespace llvm;
 
@@ -24,19 +66,20 @@ struct ReplaceCallWithLoadPass : public PassInfoMixin<ReplaceCallWithLoadPass> {
                     if (!Callee) continue;
 
                     // Check if the called function is the one we want to replace
-                    if (Callee->getName() == "external_func") {
+                    if (Callee->getName().endswith("flow_signal_get")) {
                         IRBuilder<> Builder(Call);
-
-                        // Assume return type is i32
-                        Type *RetType = Call->getType();
-                        Constant *AddrInt = ConstantInt::get(Type::getInt64Ty(Ctx), 0x12345678);
-                        Value *Ptr = Builder.CreateIntToPtr(AddrInt, RetType->getPointerTo());
-                        Value *LoadedVal = Builder.CreateLoad(RetType, Ptr);
-
+                        Type *RetType = Call->getType(); //we are inferring here the type of a signal - if the original code was wrong we will be fucked
+			Type *RetTypePtr = RetType->getPointerTo();
+                        Argument* Signals = std::next(F.arg_begin(), signals_pos); //the signals void* we need to cast and offset
+                        Value *CastPtr = Builder.CreateBitCast(Signals, RetTypePtr); //cast signals to int32* or int64*
+                        Value *Offset = Call->getArgOperand(1); //0th arg is the ctx, 1st the offset we need
+                        Value *S = Builder.CreateInBoundsGEP(RetType, CastPtr, Offset); //get *signals[offset]
+                        Value *LoadedVal = Builder.CreateLoad(RetType, S);
                         Call->replaceAllUsesWith(LoadedVal);
                         Call->eraseFromParent();
                         Changed = true;
                     }
+
                 }
             }
         }
