4b12ebf9e75e281190c8243eed40f7f5e6301dbe
Mikhail Khalilov <miharulidze@gmail.com>
2025-10-02 10:29:38 +0200

    update EQDS prototype

--- pcm/algorithms/eqds.c
+++ pcm/algorithms/eqds.c
@@ -1,6 +1,15 @@
 #include "algo_utils.h"
 #include "pcmh.h"
 
+#define CTRL_CUMULATIVE_CREDIT 0
+#define SIG_CREDIT_TARGET 1
+#define CONST_K 1
+#define CONST_MTU 1
+#define MAX_CWND 10
+#define SIG_CREDIT_TIMER 10
+#define PCM_SIG_TRIGGER_DISABLE 10
+
+
 // per-flow trigger: sig_credit_target with threshold of 1
 // threshold = sig_credit_target + 1 is adjusted dynamically
 // every time cumulative_credit - credit_target >= max_cwnd
@@ -28,13 +37,14 @@ int algorithm_main() {
     if (cumulative_credit - credit_target >= MAX_CWND) {
         // source was granted with speculative budget,
         // therefore this flow can be unsubscribed from the shared timer for now
-        set_signal(SIG_CREDIT_TIMER, PCM_SIG_NO_TRIGGER);
-        // serve this source again as soon as starts to consume credit
+        set_trigger_threshold(SIG_CREDIT_TIMER, PCM_SIG_TRIGGER_DISABLE);
+        // serve this source again as soon as it starts to consume credit
         set_trigger_threshold(SIG_CREDIT_TARGET, get_signal(SIG_CREDIT_TARGET) + 1);
     } else if (get_signal_trigger_mask() & SIG_CREDIT_TARGET) {
-        // source still needs credit and subscribes to the shared timer
+        // source still needs credit and keeps shared timer subscription
         set_trigger_threshold(SIG_CREDIT_TIMER, PCM_SIG_REARM);
     }
 
     return 0;
 }
+
