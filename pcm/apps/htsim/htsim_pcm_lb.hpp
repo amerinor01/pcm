d51fc48aa553e66fe11ef74727b2750f3fc49f23
Mikhail Khalilov <miharulidze@gmail.com>
2025-12-01 11:32:44 +0100

    lookup IO slab dynamically

--- pcm/apps/htsim/htsim_pcm_lb.hpp
+++ pcm/apps/htsim/htsim_pcm_lb.hpp
@@ -15,8 +15,7 @@ class UecPcmMp : public UecMultipath, public PcmScheduledContext {
   public:
     UecPcmMp(bool debug, PcmScheduler &scheduler, PcmScheduler::PcmVmTag tag)
         : UecMultipath(debug), _scheduler{scheduler},
-          _pcm_vm_id{_scheduler.createVm(this, tag)},
-          _pcm_io_slab{_scheduler.getVm(_pcm_vm_id).get_signal_io_slab()} {
+          _pcm_vm_id{_scheduler.createVm(this, tag)} {
         std::cout << "pcm_htsim::UecPcmMp: initialization completed"
                   << std::endl;
     }
@@ -27,18 +26,19 @@ class UecPcmMp : public UecMultipath, public PcmScheduledContext {
         // std::cout << "pcm_htsim::UecPcmMp: Process EV: " << path_id <<
         // std::endl;
 
+        auto* io_slab = _scheduler.getVm(_pcm_vm_id).get_signal_io_slab();
         switch (feedback) {
         case PATH_GOOD:
-            _pcm_io_slab->in.ack = 1;
-            _pcm_io_slab->in.ack_ev = static_cast<pcm_uint>(path_id);
+            io_slab->in.ack = 1;
+            io_slab->in.ack_ev = static_cast<pcm_uint>(path_id);
             break;
         case PATH_ECN:
-            _pcm_io_slab->in.ecn = 1;
-            _pcm_io_slab->in.ecn_ev = static_cast<pcm_uint>(path_id);
+            io_slab->in.ecn = 1;
+            io_slab->in.ecn_ev = static_cast<pcm_uint>(path_id);
             break;
         case PATH_NACK:
-            _pcm_io_slab->in.nack = 1;
-            _pcm_io_slab->in.nack_ev = static_cast<pcm_uint>(path_id);
+            io_slab->in.nack = 1;
+            io_slab->in.nack_ev = static_cast<pcm_uint>(path_id);
             break;
         case PATH_TIMEOUT:
             /* RTO is not supported yet */
@@ -64,7 +64,8 @@ class UecPcmMp : public UecMultipath, public PcmScheduledContext {
                          uint64_t cur_cwnd_in_pkts) override {
         (void)seq_sent;         // Suppress unused parameter warning
         (void)cur_cwnd_in_pkts; // Suppress unused parameter warning
-        _pcm_io_slab->in.tx_ready_pkts = 1;
+        auto* io_slab = _scheduler.getVm(_pcm_vm_id).get_signal_io_slab();
+        io_slab->in.tx_ready_pkts = 1;
         _scheduler.getVm(_pcm_vm_id).flush_slab_input();
 
         if (_scheduler.schedulerTypeGet() == PcmScheduler::ProgressType::SYNC) {
@@ -75,17 +76,17 @@ class UecPcmMp : public UecMultipath, public PcmScheduledContext {
         }
 
         _scheduler.getVm(_pcm_vm_id).fetch_slab_output();
+        io_slab = _scheduler.getVm(_pcm_vm_id).get_signal_io_slab();
 
         std::cout << "pcm_htsim::UecPcmMp:" << this << " Generate EV: "
-                  << static_cast<uint16_t>(_pcm_io_slab->out.ev) << std::endl;
+                  << static_cast<uint16_t>(io_slab->out.ev) << std::endl;
         return static_cast<uint16_t>(
-            static_cast<uint16_t>(_pcm_io_slab->out.ev));
+            static_cast<uint16_t>(io_slab->out.ev));
     }
 
   private:
     PcmScheduler &_scheduler;
     PcmScheduler::PcmVmId _pcm_vm_id;
-    pcm_vm::PcmHandlerVmDesc::PcmHandlerVmIoSlab *_pcm_io_slab;
     bool _is_finished{false};
 };
 
--- pcm/apps/htsim/htsim_pcm_src.hpp
+++ pcm/apps/htsim/htsim_pcm_src.hpp
@@ -212,8 +212,7 @@ class PcmSrc final : public UecSrc, public PcmScheduledContext {
            bool rts = false)
         : UecSrc{trafficLogger, eventList,   std::move(mp),
                  nic,           no_of_ports, rts},
-          _scheduler{scheduler}, _pcm_vm_id{_scheduler.createVm(this, tag)},
-          _pcm_io_slab{_scheduler.getVm(_pcm_vm_id).get_signal_io_slab()} {
+          _scheduler{scheduler}, _pcm_vm_id{_scheduler.createVm(this, tag)} {
 
         // Assign PCM function pointers for congestion control callbacks
         // Use proper member function pointer assignment syntax
@@ -232,7 +231,8 @@ class PcmSrc final : public UecSrc, public PcmScheduledContext {
                                         "CONTROL FETCH CYCLE");
         PCM_PERF_PROF_REGION_START(ctrl_fetch_cycle);
         _scheduler.getVm(_pcm_vm_id).fetch_slab_output();
-        UecSrc::_cwnd = _pcm_io_slab->out.cwnd;
+        auto* io_slab = _scheduler.getVm(_pcm_vm_id).get_signal_io_slab();
+        UecSrc::_cwnd = io_slab->out.cwnd;
         UecSrc::set_cwnd_bounds();
         PCM_PERF_PROF_REGION_END(ctrl_fetch_cycle, true);
 
@@ -270,12 +270,13 @@ class PcmSrc final : public UecSrc, public PcmScheduledContext {
         PCM_PERF_PROF_REGION_SCOPE_INIT(ack_registration_cycle,
                                         "ACK REGISTRATION CYCLE");
         PCM_PERF_PROF_REGION_START(ack_registration_cycle);
-        _pcm_io_slab->in.ack = 1;
-        _pcm_io_slab->in.ecn = skip ? 1 : 0;
-        _pcm_io_slab->in.data_tx = newly_acked_bytes;
-        _pcm_io_slab->in.rtt = UecSrc::_raw_rtt;
-        _pcm_io_slab->in.in_flight = UecSrc::_in_flight;
-        _pcm_io_slab->in.tx_backlog_bytes = UecSrc::_backlog;
+        auto* io_slab = _scheduler.getVm(_pcm_vm_id).get_signal_io_slab();
+        io_slab->in.ack = 1;
+        io_slab->in.ecn = skip ? 1 : 0;
+        io_slab->in.data_tx = newly_acked_bytes;
+        io_slab->in.rtt = UecSrc::_raw_rtt;
+        io_slab->in.in_flight = UecSrc::_in_flight;
+        io_slab->in.tx_backlog_bytes = UecSrc::_backlog;
         _scheduler.getVm(_pcm_vm_id).flush_slab_input();
         PCM_PERF_PROF_REGION_END(ack_registration_cycle, true);
         if (_scheduler.schedulerTypeGet() == PcmScheduler::ProgressType::SYNC) {
@@ -296,9 +297,10 @@ class PcmSrc final : public UecSrc, public PcmScheduledContext {
         PCM_PERF_PROF_REGION_SCOPE_INIT(nack_registration_cycle,
                                         "NACK REGISTRATION CYCLE");
         PCM_PERF_PROF_REGION_START(nack_registration_cycle);
-        _pcm_io_slab->in.nack = 1;
-        _pcm_io_slab->in.data_nacked = nacked_bytes;
-        _pcm_io_slab->in.rtt = UecSrc::_base_rtt + UecSrc::_network_rtt;
+        auto* io_slab = _scheduler.getVm(_pcm_vm_id).get_signal_io_slab();
+        io_slab->in.nack = 1;
+        io_slab->in.data_nacked = nacked_bytes;
+        io_slab->in.rtt = UecSrc::_base_rtt + UecSrc::_network_rtt;
         _scheduler.getVm(_pcm_vm_id).flush_slab_input();
         PCM_PERF_PROF_REGION_END(nack_registration_cycle, true);
         if (_scheduler.schedulerTypeGet() == PcmScheduler::ProgressType::SYNC) {
@@ -311,7 +313,6 @@ class PcmSrc final : public UecSrc, public PcmScheduledContext {
   private:
     PcmScheduler &_scheduler;
     PcmScheduler::PcmVmId _pcm_vm_id;
-    pcm_vm::PcmHandlerVmDesc::PcmHandlerVmIoSlab *_pcm_io_slab;
 #ifdef ENABLE_PROFILING
     pcm_uint _runtime_call_perftest;
 #endif
